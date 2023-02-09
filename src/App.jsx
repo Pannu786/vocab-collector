7c9d68785a1cc00e6a3eda91ecf27d4ba7cfb19d
Pannu786 <thepannu786@gmail.com>
2023-02-09 23:57:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from '@mui/material';
+
+const App = () => {
+  return (
+    <div>
+      <Button variant='contained'>THIs is an MUI Button</Button>
+    </div>
+  );
+};
+
+export default App;
