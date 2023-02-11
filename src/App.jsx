fa6ca24659b96b1e0231e1daebbdf9685fd23636
Pannu786 <thepannu786@gmail.com>
2023-02-11 23:06:29 +0530

    something has started idk what but something has..

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-import { Button } from '@mui/material';
+import { Typography, Box, Container } from '@mui/material';
+import Main from './components/Main';
 
 const App = () => {
   return (
-    <div>
-      <Button variant='contained'>THIs is an MUI Button</Button>
-    </div>
+    <Box
+      sx={{
+        position: 'absolute',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        width: '100%',
+        backgroundImage: `url('https://wallpaperset.com/w/full/6/7/6/211879.jpg')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+      }}
+    >
+      <Container sx={{ textAlign: 'center', mb:'auto' }}>
+        <Typography variant='h5' color='white'>
+          Vacab Collector{' '}
+        </Typography>
+        <Main />
+      </Container>
+      
+    </Box>
   );
 };
 
---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Main = () => {
+  return <div>
+    <p>This is a Main Page!!</p>
+  </div>;
+};
+
+export default Main;
